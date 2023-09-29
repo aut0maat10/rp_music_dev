dd068090b6bb75b36021066440c18840937c5136
Robin Pahlman <robin.pahlman@gmail.com>
2023-09-28 21:54:08 -0400

    more stuff

--- rp_music_dev_backend/blog/settings.py
+++ rp_music_dev_backend/blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
