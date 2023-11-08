4a96f8aa6d6cc1eed94fa4dc67d44c27049b5110
Robin Pahlman <robin.pahlman@gmail.com>
2023-11-08 11:08:54 -0500

    edit media root

--- rp_music_dev_backend/blog/settings.py
+++ rp_music_dev_backend/blog/settings.py
@@ -135,7 +135,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/ubuntu/rp_music_dev/db_location/db.sqlite3',
+        # 'NAME': '/home/ubuntu/rp_music_dev/db_location/db.sqlite3',
+        'NAME': '/home/development/code/rp_music_dev/db_location/db.sqlite3',
+
     }
     # "default": {
     #     "ENGINE": "django.db.backends.mysql",
@@ -182,11 +184,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, / 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
